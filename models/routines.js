6d39c3abd02cc7cecc0a8412b3679c4d50580de0
Joe Hinkley <joehhinkley@gmail.com>
2019-04-16 17:28:28 -0700

    updated routines model schema

--- models/routines.js
+++ models/routines.js
@@ -3,7 +3,18 @@ const Schema = mongoose.Schema;
 
 const exerciseSchema = new Schema({
   name: { type: String, required: true },
-  image: { type: String, required: true },
+  images: { 
+    still: {
+      sm: {type: String, required: true},
+      med: {type: String, required: true},
+      lrg: {type: String, required: true}
+    },
+    animated: {
+      sm: {type: String, required: true},
+      med: {type: String, required: true},
+      lrg: {type: String, required: true}
+    }
+  },
   duration: { type: Number, required: true, default: 30 }
 })
 
